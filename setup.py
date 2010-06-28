8d2215d623689aee96d0664c2520a640c03b9610
David Fraser <davidf@sjsoft.com>
2010-06-28 10:58:54 +0200

    Adjusted spacing

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from setuptools import setup
 
 VERSION = '0.1'
 
-setup(  name="dbghelper", version=VERSION,
-        author = "David Fraser",
-        author_email = 'davidf@sjsoft.com',
-	url = 'http://bitbucket.org/davidfraser/python-dbghelper',
-        description = 'Simple helper for inline winpdb/pdb debugging',
-        license='BSD',
-	py_modules=['dbg'],
-        install_requires = ['winpdb'],
+setup(
+    name="dbghelper",
+    version=VERSION,
+    author = "David Fraser",
+    author_email = 'davidf@sjsoft.com',
+    url = 'http://bitbucket.org/davidfraser/python-dbghelper',
+    description = 'Simple helper for inline winpdb/pdb debugging',
+    license='BSD',
+    py_modules=['dbg'],
+    install_requires = ['winpdb'],
 )
 

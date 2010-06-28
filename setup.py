95bc94c1c280bab96b628f552ca673c2f32f6af5
David Fraser <davidf@sjsoft.com>
2010-06-28 10:30:21 +0200

    Added setup script using easy_install

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(  name="dbghelper", version=VERSION,
+        author = "David Fraser",
+        author_email = 'davidf@sjsoft.com',
+	url = 'http://bitbucket.org/davidfraser/python-dbghelper',
+        description = 'Simple helper for inline winpdb/pdb debugging',
+        license='BSD',
+	modules=['dbg'],
+        install_requires = ['winpdb'],
+)
+
